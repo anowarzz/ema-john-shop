f3e55e57d3c1ec79d6321defaec8ead6b85c560b
anowarzz <anowarhosen444@gmail.com>
2022-10-04 11:59:54 +0600

    navbar added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import './App.css';
+import Header from './components/Header/Header';
 
 function App() {
   return (
     <div className="App">
-         <h1 className="text-5xl font-bold underline">
-      Hello world!
-    </h1>
-
+     <Header />
 
 
     
---
+++ src/components/Header/Header.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import logo from '../../images/Logo.svg'
+
+const Header = () => {
+    return (
+        <div className='bg-[#1C2B35] flex justify-between px-16 py-6 items-center flex-wrap'>
+
+        <img src={logo} alt="" />
+        <div className='text-white flex gap-4 font-semibold flex-wrap'>
+            <a className='hover:bg-amber-500 px-2 py-1 rounded-lg' href='/shop'>Shop</a>
+            <a className='hover:bg-amber-500 px-2 py-1 rounded-lg' href='/orders'>Orders</a>
+            <a className='hover:bg-amber-500 px-2 py-1 rounded-lg' href='/inventory'>Inventory</a>
+            <a className='hover:bg-amber-500 px-2 py-1 rounded-lg' href='/about'>About</a>
+        </div>
+
+        </div>
+    );
+};
+
+export default Header;
