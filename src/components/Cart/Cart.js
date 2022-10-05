39173055f3ca01a8ed66f4b37f247a93361f715e
anowarzz <anowarhosen444@gmail.com>
2022-10-05 18:34:34 +0600

    price, tax, shipping added

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,13 +1,28 @@
 import React from 'react';
 
 const Cart = ({cart}) => {
+
+
+let total = 0;
+let shipping = 0;
+
+for(const product of cart){
+    total = total + product.price;
+    shipping = shipping + product.shipping;
+
+}
+
+
+
+const tax = (total * 0.1).toFixed(2);
+
     return (
         <div className='md:sticky top-6'>
             <h3 className='text-3xl font-semibold text-center mt-6'>Order Summary</h3>
             <p className='mt-3 pl-5'>Selected Items: {cart.length}</p>
-            <p className='mt-3 pl-5'>Total Price : </p>
-            <p className='mt-3 pl-5'>Total Shipping: </p>
-            <p className='mt-3 pl-5'>Tax : </p>
+            <p className='mt-3 pl-5'>Total Price : ${total} </p>
+            <p className='mt-3 pl-5'>Total Shipping : ${shipping} </p>
+            <p className='mt-3 pl-5'>Tax : ${tax} </p>
             <h5 className='mt-3 pl-5 text-2xl font-semibold'>Grand Total : </h5>
         </div>
     );
