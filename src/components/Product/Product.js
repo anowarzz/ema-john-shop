8d4c07200e2f076e87bae2710d3b5a7e5d6c071f
anowarzz <anowarhosen444@gmail.com>
2022-10-05 10:05:23 +0600

    card information added

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -3,19 +3,27 @@ import React from 'react';
 const Product = ({product}) => {
   const {name, price, img, seller, ratings} = product;
     return (
-        <div>
-         <div className="card bg-gray-300 shadow border border-zinc-200">
-  <figure><img src={img} alt="Shoes" /></figure>
-  <div className="m-2">
-    <h2 className="card-title">Shoes!</h2>
-    <p>If a dog chews shoes whose shoes does he choose?</p>
+         <div className="rounded-lg shadow-md border h-[510px] relative">
  
-      <button className="btn bg-[#276964] w-full">Buy Now</button>
+  <div>
 
+  <img className='mx-auto mt-3 rounded w-[286px] h-[286px]' src={img} alt="Shoes" />
+
+   <div className='px-4 mt-4'>
+   <h2 className="font-bold text-lg"> {name} </h2>
+    <p className='font-semibold pt-2 pb-1'>Price : ${price}</p>
+    <p className='pb-1'>Seller: {seller}</p>
+    <p className='pb-1'>Ratings: <span className='font-semibold'>{ratings}</span> Stars </p>
+   </div>
+ 
+ 
+     <button className="absolute bottom-0 btn border-0 bg-[#276964] w-[50%] md:w-full hover:bg-[#3e99b4] hover:text-black">Add To Cart</button>
+
+ 
   </div>
 </div>
 
-        </div>
+        
     );
 };
 
