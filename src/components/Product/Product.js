97b1de66952357c541756b822e902b0c7243d6dd
anowarzz <anowarhosen444@gmail.com>
2022-10-05 10:30:12 +0600

    button responsive now

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -11,19 +11,17 @@ const Product = ({product}) => {
           event.target.src = "https://i.ibb.co/kDjDBcn/no-image-found.jpg"
           event.onerror = null }}/>
 
-   <div className='px-4 mt-4'>
+   <div className='px-4 mt-4 text-center'>
    <h2 className="font-bold text-lg"> {name} </h2>
     <p className='font-semibold pt-2 pb-1'>Price : ${price}</p>
     <p className='pb-1'>Seller: {seller}</p>
     <p className='pb-1'>Ratings: <span className='font-semibold'>{ratings}</span> Stars </p>
+
    </div>
  
  
-    
-    <button className="mx-auto absolute bottom-0 btn border-0 bg-[#276964] w-[50%] md:w-full hover:bg-[#3e99b4] hover:text-black">Add To Cart</button>
+ <button className="absolute bottom-0 btn border-0 bg-[#276964] w-full hover:bg-[#3e99b4] hover:text-black">Add To Cart</button>
 
-
- 
   </div>
 </div>
 
