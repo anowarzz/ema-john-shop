065e3a8a32bd17caae0222cd0c839af299a7fa2f
anowarzz <anowarhosen444@gmail.com>
2022-10-07 09:36:27 +0600

    logo modified

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,7 +5,10 @@ const Header = () => {
     return (
         <div className='bg-[#1C2B35] flex justify-between px-16 py-6 items-center flex-wrap'>
 
+        <a href="/">
         <img src={logo} alt="" />
+        </a>
+
         <div className='text-white flex gap-4 font-semibold flex-wrap'>
             <a className='hover:bg-amber-500 px-2 py-1 rounded-lg' href='/shop'>Shop</a>
             <a className='hover:bg-amber-500 px-2 py-1 rounded-lg' href='/orders'>Orders</a>
